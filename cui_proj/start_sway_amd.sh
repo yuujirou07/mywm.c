94efe69463efa63842c55b96089a6d210bd6ea27
ryuki <rmer0548@gmail.com>
2026-08-13 23:49:21 +0900

    add code

--- cui_proj/start_sway_amd.sh
+++ cui_proj/start_sway_amd.sh
@@ -18,6 +18,11 @@ fi
 
 export WLR_DRM_DEVICES="$amdgpu_card"
 export SWAY_UNSUPPORTED_GPU=true
+
+# IME (fcitx5)。sway の設定ファイル内では export できないのでここで設定する。
+# Wayland ネイティブなアプリは text-input プロトコルで動くため、
+# XWayland / X11 アプリ向けに XMODIFIERS だけ渡す。
+export XMODIFIERS=@im=fcitx
 unset __GLX_VENDOR_LIBRARY_NAME
 unset __NV_PRIME_RENDER_OFFLOAD
 unset __VK_LAYER_NV_optimus
